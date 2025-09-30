5c4f90700a8ff0c5d19f90b26ef7c69e0b5712f3
DIEGO <DIEGO@DESKTOP-RT13KO6>
2025-09-29 20:47:39 -0500

    carga inicial

--- src/programas/Operaciones.java
+++ src/programas/Operaciones.java
@@ -4,7 +4,7 @@ package programas;
 import java.util.Scanner;
 public class Operaciones {
   public static void main(String[] args){
-    int n1,n2,suma;
+    int n1,n2,suma,rest;
     Scanner lectura=new Scanner(System.in);
     
     System.out.println("Ingresar Numero 1:");
@@ -13,8 +13,9 @@ public class Operaciones {
     n2=lectura.nextInt();
     
     suma=n1+n2;
+    rest=n1-n2;
     
     System.out.println("La suma de los numeros es:" +suma);
-    
+    System.out.println("La resta de los numeros es:" +rest);
   }  
 }
